f06d561472d0e766cd71704a0fcc4ff0a222c232
Hanna Shymanska <a.shymanska.ip13@kpi.ua>
2022-06-23 18:17:19 +0300

    Validation added, filecontent validated

--- lab 4/ImageResizer/FilePathValidator.cs
+++ lab 4/ImageResizer/FilePathValidator.cs
@@ -1,15 +1,20 @@
-﻿namespace ImageResizer
+﻿using System;
+using System.Globalization;
+using System.IO;
+
+namespace ImageResizer
 {
     public static class FilePathValidator
     {
         public static bool Validate(string path)
         {
-            return true;
+            return (File.Exists(path) && !String.IsNullOrEmpty(path));
         }
 
         public static bool FileDirectoryExists(string path)
         {
-            return true;
+            string dir = Path.GetDirectoryName(path);
+            return Directory.Exists(dir);
         }
     }
 }
--- lab 4/ImageResizer/Program.cs
+++ lab 4/ImageResizer/Program.cs
@@ -6,18 +6,18 @@ namespace ImageResizer
     {
         static void Main(string[] args)
         {
-            foreach (var VARIABLE in args)
-            {
-                Console.WriteLine(VARIABLE);
-            }
             if (args.Length != 3 || !FilePathValidator.Validate(args[0]) ||
                 !FilePathValidator.FileDirectoryExists(args[1]) ||
                 ! double.TryParse(args[2], out double multiplier)) return;
-            Console.WriteLine("Resizing...");
+            Console.WriteLine($"Enlarging image {multiplier} times... Done.");
             byte[] content = FileInput.GetContent(args[0]);
-            Picture pic = new Picture(content);
-            pic.Resize(multiplier);
-            FileOutput.ImageToFile(pic, args[1]);
+            if (FileValidator.ValidateFile(content))
+            {
+                Picture picture = new Picture(content);
+                picture.Resize(multiplier);
+                FileOutput.ImageToFile(picture, args[1]);
+            }
+            else Console.WriteLine("Incorrect file structure");
         }
     }
 }
